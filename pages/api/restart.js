38287ab5adaff66b4ec68ac38b39fac74c341988
devbrock <tbrockb@gmail.com>
2023-02-09 20:00:07 -0600

    update command

---
+++ pages/api/restart.js
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+let SSH = require('simple-ssh')
+
+
+export default function handler(req, res) {
+    const ssh = new SSH({
+        host: '170.187.201.49',
+        user: 'root',
+        pass: 'JettaMeeko1009'
+    })
+
+    ssh.exec('docker restart twilioTest', {
+        out: console.log.bind(console)
+    }).start();
+
+    res.status(200).json({ command: 'restart' })
+}
